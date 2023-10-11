e94c5a0ae760ff4c4d528a617313ee4fabf578df
samuel21695 <samuel21695@gmail.com>
2023-10-11 10:53:25 +0900

    create array with function student

--- newfile.js
+++ newfile.js
@@ -1,7 +1,21 @@
+// 객체를 만드는 것이 아닌 뽑아내는 것
 function student(name, number) {
   this.name = name;
   this.number = number;
 };
 
-let test = new student("소사무엘", "5");
-console.log(test);
+let studentList = ["소사무엘", "유승민"];
+let studentArray = [];
+for (i=0; i < studentList.length; i++) {
+  studentArray[i] = new student(studentList[i], i+1);
+};
+console.log(studentArray);
+
+// let test = new student("소사무엘", "5");
+// console.log(test);
+
+// // 변수를 그냥 만든 것
+// let studentList = {
+//   name : "hello",
+//   number : 1,
+// }
