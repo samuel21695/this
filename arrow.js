580c4dfaccc981a5f45e74e84b33e108c89f1121
samuel21695 <samuel21695@gmail.com>
2023-10-11 11:35:05 +0900

    two other but same function

--- arrow.js
+++ arrow.js
@@ -1,9 +1,14 @@
-function outer() {
-
-};
-
 // 두 함수의 차이점은 무엇일까
 
+function outer() {
+  return "hello";
+};
+
+
+// 이름이 있는 듯 하지만 없는 "익명 함수" = "함수 리터럴"
 const outerTwo = function () {
-  
-}
+  return "bye"
+};
+
+console.log(outer);
+console.log(outerTwo);
